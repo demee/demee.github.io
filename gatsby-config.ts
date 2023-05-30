9b6a0b9c21bedaa74d839896786aaffa1dd4cda0
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2023-05-30 22:01:46 +0000

    Passign build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-google-gtag", {
+  plugins: ["gatsby-plugin-emotion", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
