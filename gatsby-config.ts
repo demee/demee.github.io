cccd11eeded26d1130540d69911d028582fc449d
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2023-06-15 19:28:52 +0000

    favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,10 +9,12 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", {
+  plugins: ["gatsby-plugin-emotion", 
+  "gatsby-source-contentful",
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/images/favicon.png"
     }
   }]
 };
