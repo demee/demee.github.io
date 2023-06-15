5e80abb0814aacbbedff17176d62f6c13dfc7be7
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2023-06-15 17:57:13 +0000

    Changed title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -146,4 +146,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>demee.org</title>
