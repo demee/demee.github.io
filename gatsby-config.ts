8e2c1a2c29d817c3d6f922e9934fbeb656df2bc2
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2023-07-25 01:09:46 +0100

    Added eslint

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,38 +1,39 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `demee.org`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'demee.org',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // More easily incorporate content into your pages through automatic
+  // TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-  "gatsby-plugin-emotion", 
-  "gatsby-plugin-image",
-  {
-    resolve: `gatsby-source-contentful`,
-    options: {
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
-  },
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/favicon.png",
-      "name": "demee.org",
-      "short_name": "demee.org",
-      "start_url": "/",
-      "background_color": "#ffffff",
-    }
-  }]
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon.png',
+        name: 'demee.org',
+        short_name: 'demee.org',
+        start_url: '/',
+        background_color: '#ffffff',
+      },
+    }],
 };
 
 export default config;
