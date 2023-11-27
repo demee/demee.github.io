8b372e705932f5ba534f9378fa270d72fefc0d59
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2023-11-27 02:16:49 +0000

    Types for queries

---
+++ src/pages/typegen.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { graphql, PageProps } from 'gatsby';
+
+const pageStyles = {
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+};
+
+const TypegenPage = ({ data }: PageProps<Queries.TypegenPageQuery>) => (
+    <main style={pageStyles}>
+      <p>Site title: {data.site?.siteMetadata?.title}</p>
+      <hr />
+      <p>Query Result:</p>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </main>
+);
+
+export default TypegenPage;
+
+export const query = graphql`
+  query TypegenPage {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
+
